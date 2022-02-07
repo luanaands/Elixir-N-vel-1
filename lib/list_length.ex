722c26202d33a21cc5232045fa6d22a3bc51bbdd
luana <luana.santana@celcoin.com.br>
2022-02-06 22:44:15 -0300

    criei funcao recursiva

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,11 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: count(list, 0)
+
+  defp count([], qtd) do
+    qtd
+  end
+
+  defp count([_head | teil], qtd) do
+    count(teil, qtd + 1)
   end
 end
